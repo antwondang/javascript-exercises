eba9405ecea9284de14606a173d549780001bc6f
Anthony Dang <anthonydang1999@gmail.com>
2023-09-20 21:41:47 -0700

    Undo modifications in exercise 1,2,3 to original file state

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,5 @@
-const repeatString = (string, num) => {
-    if (num < 0)
-        return "ERROR";
-    if (num === 1)
-        return string;
-    if (num === 0)
-        return "";
-    else 
-        return string + repeatString(string, num - 1);
-};
-
-console.log(repeatString("hey", 0))
+const repeatString = function() {
+}
 
 // Do not edit below this line
 module.exports = repeatString;
