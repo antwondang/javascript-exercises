1766719a6b584115bbb3723d62fae5ee1ca1c482
Anthony Dang <anthonydang1999@gmail.com>
2023-09-20 21:19:23 -0700

    Create reverse string function in reverseString.js and pass test cases in reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+    return string.split("").reverse().join("");
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
