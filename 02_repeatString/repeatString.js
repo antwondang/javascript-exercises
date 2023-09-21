3ed7f2d1edb80e303e6677ba4c942af88d26ac61
Anthony Dang <anthonydang1999@gmail.com>
2023-09-20 21:11:10 -0700

    Create repeat string function in repeatString.js and pass test cases in repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+    if (num < 0)
+        return "ERROR";
+    if (num === 1)
+        return string;
+    if (num === 0)
+        return "";
+    else 
+        return string + repeatString(string, num - 1);
 };
+
+console.log(repeatString("hey", 0))
 
 // Do not edit below this line
 module.exports = repeatString;
